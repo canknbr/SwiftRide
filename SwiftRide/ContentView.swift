887ce53296cdbe97b2690ec61fa364714ecf90e8
canknbr <cankanbur1@gmail.com>
2023-06-05 18:06:33 +0300

    Initial Commit

---
+++ SwiftRide/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftRide
+//
+//  Created by Can Kanbur on 5.06.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftRide/SwiftRideApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftRideApp.swift
+//  SwiftRide
+//
+//  Created by Can Kanbur on 5.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftRideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
